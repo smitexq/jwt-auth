63da88c59cc52b6ee40ac92527fc69888f177f7a
smitexq <spygidak@gmail.com>
2025-05-12 16:24:44 +0300

    Закончил jwt service

--- src/main/java/com/jwt_auth/jwt/security/JwtService.java
+++ src/main/java/com/jwt_auth/jwt/security/JwtService.java
@@ -1,9 +1,12 @@
 package com.jwt_auth.jwt.security;
 
 import com.jwt_auth.jwt.dto.JwtAuthenticationDto;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +20,56 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String jwtsecret;
 
+    private static final Logger logger = LogManager.getLogger(JwtService.class);
+
+    //Оба токена не валиды, происходит генерации обоих
     public JwtAuthenticationDto generateAuthToken(String email) {
         JwtAuthenticationDto jwtDTO = new JwtAuthenticationDto();
         jwtDTO.setToken(generateJwtToken(email));
         jwtDTO.setRefreshToken(generateRefreshToken(email));
+        return jwtDTO;
     }
 
+    public JwtAuthenticationDto refreshBaseToken(String email, String refreshToken) {
+        JwtAuthenticationDto jwtDTO = new JwtAuthenticationDto();
+        jwtDTO.setRefreshToken(refreshToken);
+        jwtDTO.setToken(generateJwtToken(email));
+        return jwtDTO;
+    }
+
+    public String getEmailFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSingInKey())
+                .build()
+                .parseEncryptedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSingInKey())
+                    .build()
+                    .parseEncryptedClaims(token)
+                    .getPayload();
+            return true;
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired", e);
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported", e);
+        } catch (MalformedJwtException e) {
+            logger.error("Malformed", e);
+        } catch (SecurityException e) {
+            logger.error("Security", e);
+        } catch (Exception e) {
+            logger.error("Invalid token", e);
+        }
+        return false;
+    }
+
+
+    //Временный токен (генерируется за счет постоянного)
     private String generateJwtToken(String email) {
         Date date = Date.from(LocalDateTime.now().plusSeconds(30).atZone(ZoneId.systemDefault()).toInstant());
         return Jwts.builder()
@@ -31,7 +78,7 @@ public class JwtService {
                 .signWith(getSingInKey())
                 .compact();
     }
-
+    //Генерация постоянного токена
     private String generateRefreshToken(String email) {
         Date date = Date.from(LocalDateTime.now().plusMinutes(3).atZone(ZoneId.systemDefault()).toInstant());
         return Jwts.builder()
